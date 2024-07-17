2e162c8372fcf43517e7653d4beea5506d81da4e
chrismugabo <cmugabo5@gmail.com>
2024-07-17 14:03:52 -0400

    changed some files

--- higher_game.py
+++ higher_game.py
@@ -1,5 +1,21 @@
-# from art import logo
-# print(logo)
+from art import logo
+from art import vs
+print(logo)
+import random
 from game_data import data
+ 
+# def compare(guess):
 
-print(len(data))
+random1=random.choice(data)
+print(f"Compare A: {random1['name']}, a {random1['description']} from {random1['country']} " )
+print(vs)
+random2=random.choice(data)
+print(f"Against B: {random2['name']}, a {random2['description']} from {random2['country']} " )
+
+choice=input("Who has more followers? Type 'A' or 'B':")
+
+
+
+
+
+ 
